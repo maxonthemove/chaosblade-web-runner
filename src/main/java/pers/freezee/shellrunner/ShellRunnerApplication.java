0f28c91268521f96a2505dac05a8189e5a741795
王东杰 <wangdongjie@ucfgroup.com>
2019-03-29 17:07:47 +0800

    init

---
+++ src/main/java/pers/freezee/shellrunner/ShellRunnerApplication.java
@@ -0,0 +1,13 @@
+package pers.freezee.shellrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShellRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShellRunnerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pers/freezee/shellrunner/ShellRunnerApplicationTests.java
@@ -0,0 +1,16 @@
+package pers.freezee.shellrunner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShellRunnerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
